5369771b08f7dc9959cfc74d34480491bc0384f5
hardyreinhold <hardykaasik@gmail.com>
2025-05-19 11:40:45 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.pank_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pank_java to javafx.fxml;
+    exports com.example.pank_java;
+    exports com.example.pank_java.controllers;
+    opens com.example.pank_java.controllers to javafx.fxml;
+}
